dca34065a2a201b3176e5f97f1c0d0fdaff09912
jonas-Jun <junhot08@gmail.com>
2021-08-08 11:01:44 +0900

    210808 DFS, recursion use only nonlocal

--- leet_130_surroundRegions.py
+++ leet_130_surroundRegions.py
@@ -20,7 +20,6 @@ def solve(board) -> None:
 
     def dfs_helper(i,j):
         nonlocal board, visited_map, path, check, m, n
-        if visited_map[i][j]: return
         path.append((i,j))
         visited_map[i][j] = True
         if i == 0 or j == 0 or i == m-1 or j == n-1:
@@ -45,13 +44,44 @@ def solve(board) -> None:
         board[loc[0]][loc[1]] = 'X'
     return
 
+def solve2(board) -> None:
+    '''
+    보더라인에서 출발하는 dfs만 O로 남기고 나머지는 X처리
+    '''
+    m, n = len(board), len(board[0])
+    visited_map = [[False for k in range(n)] for l in range(m)]
+    path = list()
+    
+    def dfs_helper(i,j):
+        nonlocal board, visited_map, path, m, n
+        path.append((i,j))
+        visited_map[i][j] = True
+
+        togo = [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]
+        for loc in togo:
+            idx_i, idx_j = loc[0], loc[1]
+            if 0 <= idx_i < m and 0 <= idx_j < n and not visited_map[idx_i][idx_j] and board[idx_i][idx_j] == 'O':
+                dfs_helper(idx_i, idx_j)
+    
+    for i in range(m):
+        for j in range(n):
+            if board[i][j] == 'O' and not visited_map[i][j] and (i==0 or j==0 or i==m-1 or j==n-1):
+                dfs_helper(i,j)
+
+    path = set(path)            
+    for i in range(m):
+        for j in range(n):
+            if (i,j) not in path:
+                board[i][j] = 'X'
+    return
+
+
 # for test
-t1 = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
-
 from time_check import check
-
+t1 = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
 print(check(solve, t1))
-#print(check(solve2, t1))
+t1 = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+print(check(solve2, t1))
 
 print(t1)
 
