d025e42f6ae2beefaff2fad75434a87dad7db08a
jonas-Jun <junhot08@gmail.com>
2021-10-03 09:30:24 +0900

    211003 add leet_103_zigzag order

--- leet_102_levelOrder_BT.py
+++ leet_102_levelOrder_BT.py
@@ -1,6 +1,4 @@
 from typing import List
-
-
 '''
 주어진 Binary Tree의 level oreder traversal
 
@@ -25,3 +23,32 @@ def levelOrder(root) -> List:
 from Project4.BST_Helper import create_linked_bst
 t1 = [3,9,20,None,None,15,7]
 print(levelOrder(create_linked_bst(t1)))
+
+
+'''
+zigzag order level traversal
+https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal
+'''
+
+def zigzagLevelOrder(root) -> List[List[int]]:
+    if not root: return list()
+    rst = list()
+    rst.append([root.val])
+    q = list()
+    q.append([root])
+    reverse = True # 둘째줄은 reverse
+    while q:
+        level = list()
+        for node in q.pop():
+            if not node: continue
+            if node.left:
+                level.append(node.left)
+            if node.right:
+                level.append(node.right)
+        if not any(level): return rst
+        q.append(level)
+        if reverse:
+            rst.append([n.val for n in level][::-1])
+        else:
+            rst.append([n.val for n in level])
+        reverse = not reverse
